bb5daf8a71f39d83a47827ef5d73d6e2626629a8
Rabeyakhanom <abdullaabir2026@gmail.com>
2024-10-15 14:24:35 +0600

    first commit

---
+++ length.js
@@ -0,0 +1,46 @@
+//check length
+const number =[1, 2,3, 23, 43, 45, 67, 76, 55, 65, 76];
+
+//console.log(number.length);//cheack lenth
+ //cheak element
+ //console.log(numbers[6]):
+ //const x = [6];
+ //cheak element
+
+ //indexof
+ //console.log(numbersindexof(50));
+ //indexof
+
+ // //push
+ //numbers.push(90)
+ //console.log(numbers);
+ //push
+ 
+//  //push
+//  const teams = ["Abir","keya","Almin","Mitu","Abdulla","Tanveir"]
+ //teams.push(Rabeya)
+ //console.log(teams);
+ //push
+
+ //pop
+ //teams.pop("Rabeya")
+ //console.log(brands);
+ //pop
+
+ //unshift
+ const teams= ["Abir","Keya","Almin","Mitu","Abdulla","Tanveir"]
+teams.unshift("Rabeya");
+ console.log(teams);
+ //unshift
+
+ //shift
+ //console Teams= ["Abir","Keya","Almin","Mitu","Abdulla","Tanveir"]
+ //teams.shift("Rabeya");
+ //consle.log(teams);
+ //shift
+  
+
+
+
+ 
+
---
+++ script.js
@@ -0,0 +1,15 @@
+// var x = 20;
+// var y = 40;
+// var z = x + y;
+// console.log(x+y);
+
+
+const x =[1,2,3,34, 54,46,44,];
+console.log(x);
+
+
+const team= {
+    firstname: "Rabeya",
+    lastname: "Khanom",
+    age: "29",
+}
